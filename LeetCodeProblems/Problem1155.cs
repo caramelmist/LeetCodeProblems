d645c7f07ff9b25c77b408e29de1136133c36a1d
caramelmist <sidesh@mweb.co.za>
2022-10-03 21:24:11 +0200

    Started problem 1155

---
+++ LeetCodeProblems/Problem1155.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Problem1155 : Solution
+    {
+        public void Run()
+        {
+            int actual = NumRollsToTarget(1, 6, 3);
+            int expected = 1;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = NumRollsToTarget(2, 6, 7);
+            expected = 6;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = NumRollsToTarget(30, 30, 500);
+            expected = 222616187;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+        }
+
+        public int NumRollsToTarget(int n, int k, int target)
+        {
+            int result = 0;
+
+            int[] reels = new int[n];
+
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+
+                }
+            }
+
+            return result % 1000000007;
+        }
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -14,7 +14,7 @@ namespace LeetCodeProblems
             timer.Start();
 
             //B: Run stuff you want timed
-            Solution s = new Problem91();
+            Solution s = new Problem1155();
             s.Run();
 
             timer.Stop();
