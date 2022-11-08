51e255dac8fb260b6d743765b0f7ccab7871fb58
caramelmist <sidesh@mweb.co.za>
2022-11-08 12:21:38 +0200

    Added Problem 2131

---
+++ LeetCodeProblems/Problem2131.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Problem2131 : Solution
+    {
+        public void Run()
+        {
+            int actual = LongestPalindrome(new string[] { "lc", "cl", "gg" });
+            int expected = 6;
+            Console.WriteLine("Expected matches actual: {0}", expected == actual);
+
+            actual = LongestPalindrome(new string[] { "ab", "ty", "yt", "lc", "cl", "ab" });
+            expected = 8;
+            Console.WriteLine("Expected matches actual: {0}", expected == actual);
+
+            actual = LongestPalindrome(new string[] { "cc", "ll", "xx" });
+            expected = 2;
+            Console.WriteLine("Expected matches actual: {0}", expected == actual);
+        }
+
+        public int LongestPalindrome(string[] words)
+        {
+            return 0;
+        }
+    }
+}
