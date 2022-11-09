696b982c1725e8951ad641ac775da14b7e828928
Sidesh Jungbahadur <sidesh.jungbahadur@derivco.co.za>
2022-11-09 14:09:21 +0200

    Completed problem901

--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -15,7 +15,7 @@ namespace LeetCodeProblems
             timer.Start();
 
             //B: Run stuff you want timed
-            Solution s = new Problem1706();
+            Solution s = new Problem901();
             s.Run();
 
             timer.Stop();
---
+++ LeetCodeProblems/Solved/Problem901.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace LeetCodeProblems
+{
+    public class Problem901: Solution
+    {
+        public Problem901()
+        {
+        }
+
+        public void Run()
+        {
+            StockSpanner ss = new StockSpanner();
+            int value = ss.Next(100);
+            Console.WriteLine("Passed: {0}", value == 1);
+            value = ss.Next(80);
+            Console.WriteLine("Passed: {0}", value == 1);
+            value = ss.Next(60);
+            Console.WriteLine("Passed: {0}", value == 1);
+            value = ss.Next(70);
+            Console.WriteLine("Passed: {0}", value == 2);
+            value = ss.Next(60);
+            Console.WriteLine("Passed: {0}", value == 1);
+            value = ss.Next(75);
+            Console.WriteLine("Passed: {0}", value == 4);
+            value = ss.Next(85);
+            Console.WriteLine("Passed: {0}", value == 6);
+        }
+    }
+
+    public class StockSpanner
+    {
+        private List<int> _prices = new List<int>();
+
+        public StockSpanner()
+        {
+
+        }
+
+        public int Next(int price)
+        {
+            _prices.Add(price);
+            int c = 0;
+            for (int i = _prices.Count-1; i >= 0; i--)
+            {
+                if (_prices[i] > price)
+                {
+                    return c;
+                }
+                else
+                {
+                    c++;
+                }
+            }
+
+            return c;
+        }
+    }
+}
+
