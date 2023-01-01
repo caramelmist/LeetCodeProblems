0321c3b3049718afcddec86a139f47188748fbba
caramelmist <sidesh@mweb.co.za>
2023-01-01 21:47:25 +0200

    Solved 290

--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -15,7 +15,7 @@ namespace LeetCodeProblems
             timer.Start();
 
             //B: Run stuff you want timed
-            Solution s = new Problem66();
+            Solution s = new Problem290();
             s.Run();
 
             timer.Stop();
---
+++ LeetCodeProblems/Solved/Problem290.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Solved
+{
+    public class Problem290 : Solution
+    {
+        public void Run()
+        {
+            bool actual = WordPattern("abba", "dog cat cat dog");
+            bool expected = true;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = WordPattern("abba", "dog cat cat fish");
+            expected = false;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = WordPattern("aaaa", "dog cat cat dog");
+            expected = false;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+        }
+
+        public bool WordPattern(string pattern, string s)
+        {
+            string[] words = s.Split(" ");
+            if(pattern.Length != words.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, string> map = new Dictionary<char, string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!map.ContainsKey(pattern[i]))
+                {
+                    if (map.ContainsValue(words[i]))
+                    {
+                        return false;
+                    }
+                    map.Add(pattern[i], words[i]);
+                }
+
+                if (words[i] != map[pattern[i]])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
