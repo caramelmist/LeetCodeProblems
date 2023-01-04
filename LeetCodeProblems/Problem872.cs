e8fc2f8af6d6e23482f3356c9d1ab3c10931cb43
Sidesh Jungbahadur <sidesh.jungbahadur@derivco.co.za>
2023-01-04 14:43:53 +0200

    Create Problem872.cs

---
+++ LeetCodeProblems/Problem872.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCodeProblems
+{
+	public class Problem872: Solution
+	{
+		public Problem872()
+		{
+		}
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LeafSimilar(TreeNode root1, TreeNode root2)
+        {
+            return false;
+        }
+    }
+}
+
