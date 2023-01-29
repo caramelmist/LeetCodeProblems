78bc9cfcfd84d2cf970400c310ad87449ff37d54
caramelmist <sidesh@mweb.co.za>
2023-01-29 15:48:00 +0200

    Added unsolved problems

--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -15,7 +15,7 @@ namespace LeetCodeProblems
             timer.Start();
 
             //B: Run stuff you want timed
-            Solution s = new Problem149();
+            Solution s = new Problem1061();
             s.Run();
 
             timer.Stop();
