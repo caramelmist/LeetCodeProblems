b19b3fe2d77e4a1e12675223a44641415d41e5ea
Sidesh Jungbahadur <sidesh.jungbahadur@derivco.co.za>
2023-04-19 08:25:40 +0200

    Solved 1768

---
+++ LeetCodeProblems/Solved/Problem1768.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace LeetCodeProblems.Solved
+{
+	public class Problem1768: Solution
+	{
+		public Problem1768()
+		{
+		}
+
+        public void Run()
+        {
+            string actual = MergeAlternately("abc", "pqr");
+            string expected = "apbqcr";
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = MergeAlternately("ab", "pqrs");
+            expected = "apbqrs";
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = MergeAlternately("abcd", "pq");
+            expected = "apbqcd";
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+        }
+
+        public string MergeAlternately(string word1, string word2)
+        {
+            string ret = "";
+            for (int i = 0; i < word1.Length || i < word2.Length; i++)
+            {
+                if (i < word1.Length)
+                {
+                    ret += word1[i];
+                }
+                if (i < word2.Length)
+                {
+                    ret += word2[i];
+                }
+            }
+
+            return ret;
+        }
+    }
+}
+
