ba3751145f6a88309b45ecc331f860eb0a1c1441
caramelmist <sidesh@mweb.co.za>
2023-05-01 15:43:45 +0200

    Solved problem 258

---
+++ LeetCodeProblems/Solved/Problem258.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Solved
+{
+    public class Problem258 : Solution
+    {
+        public void Run()
+        {
+            int actual = AddDigits(38);
+            int expected = 2;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = AddDigits(0);
+            expected = 0;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = AddDigits(10);
+            expected = 1;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+        }
+
+        public int AddDigits(int num)
+        {
+            int sum = 0;
+            do
+            {
+                sum += num % (10);
+                num /= 10;
+                if (num == 0 && sum > 9)
+                {
+                    num = sum;
+                    sum = 0;
+                }
+            } while (num > 0);
+
+            return sum;
+        }
+    }
+}
