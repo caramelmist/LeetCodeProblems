d4824e3d9ad765779c41b4db216fd60eb24a4eb8
Sidesh Jungbahadur <sidesh.jungbahadur@derivco.co.za>
2023-05-05 15:15:37 +0200

    Added test project

---
+++ LeetCodeProblemsTest/Solved/Problem1046Test.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeProblemsTest.Solved;
+
+public class Problem1046Test
+{
+    [Test]
+    [TestCase(new int[] { 2, 7, 4, 1, 8, 1 }, 1)]
+    [TestCase(new int[] { 1 }, 1)]
+    [TestCase(new int[] { 3 }, 3)]
+    [TestCase(new int[] { 10, 6 }, 4)]
+    [TestCase(new int[] { 6, 10 }, 4)]
+    [TestCase(new int[] { 10, 10 }, 0)]
+    [TestCase(new int[] { 20, 70, 40, 10, 5, 80 }, 5)]
+    [TestCase(new int[] { 3, 7, 2 }, 2)]
+    public void Test(int[] values, int expected)
+    {
+        Problem1046 sut = new Problem1046();
+        int actual = sut.LastStoneWeight(values);
+        actual.Should().Be(expected);
+    }
+}
