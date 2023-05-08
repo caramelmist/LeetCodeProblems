20c1cefd531cfb79f52e3e279b184a63a5c6cd77
caramelmist <sidesh@mweb.co.za>
2023-05-08 22:48:38 +0200

    completed 1572 and started 1498

---
+++ LeetCodeProblems/Problem1498.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class Problem1498 : Solution
+    {
+        public void Run()
+        {
+            int actual = NumSubseq(new int[] { 3, 5, 6, 7 }, 9);
+            int expected = 4;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = NumSubseq(new int[] { 3, 3, 6, 8 }, 10);
+            expected = 6;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = NumSubseq(new int[] { 2, 3, 3, 4, 6, 7 }, 12);
+            expected = 61;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+        }
+
+        public int NumSubseq(int[] nums, int target)
+        {
+            int result = 1;
+
+            return result % 1000000007;
+        }
+
+    }
+}
---
+++ LeetCodeProblems/Solved/Problem1572.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Solved
+{
+    public class Problem1572 : Solution
+    {
+        public void Run()
+        {
+            int actual = DiagonalSum(new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 7, 8, 9 } });
+            int expected = 25;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = DiagonalSum(new int[][] { new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1, 1 } });
+            expected = 9;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = DiagonalSum(new int[][] { new int[] { 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1 } });
+            expected = 8;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = DiagonalSum(new int[][] { new int[] { 7, 3, 1, 9 }, new int[] { 3, 4, 6, 9 }, new int[] { 6, 9, 6, 6 }, new int[] { 9, 5, 8, 5 } });
+            expected = 55;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = DiagonalSum(new int[][] { new int[] { 5 } });
+            expected = 5;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+        }
+
+        public int DiagonalSum(int[][] mat)
+        {
+            int sum = 0;
+
+            for (int i = 0, j = mat.Length - 1; i < mat.Length; i++, j--)
+            {
+                sum += mat[i][i];
+                if(i != j)
+                {
+                    sum += mat[i][j];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
