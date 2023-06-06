8590f65232a6a6d25af52db7791a327b18d6399e
Sidesh Jungbahadur <sidesh.jungbahadur@derivco.co.za>
2023-06-06 12:56:57 +0200

    Solved 144

--- LeetCodeProblems/Solved/Problem144.cs
+++ LeetCodeProblems/Solved/Problem144.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetCodeProblems
+namespace LeetCodeProblems.Solved
 {
     public class Problem144 : Solution
     {
         public void Run()
         {
-            TreeNode root = new TreeNode(2, new TreeNode(3, new TreeNode(3), new TreeNode(1)), new TreeNode(1, null, new TreeNode(1)));
+            TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3)));
             IList<int> actual = PreorderTraversal(root);
             IList<int> expected = new List<int>() { 1, 2, 3 };
             Console.WriteLine("Outcome: {0}", expected.SequenceEqual(actual));
 
-            root = new TreeNode(2, new TreeNode(1, new TreeNode(1), new TreeNode(3, null, new TreeNode(1))), new TreeNode(1));
+            root = null;
             actual = PreorderTraversal(root);
             expected = new List<int>();
             Console.WriteLine("Outcome: {0}", expected.SequenceEqual(actual));
@@ -28,7 +29,28 @@ namespace LeetCodeProblems
 
         public IList<int> PreorderTraversal(TreeNode root)
         {
-            return new List<int>();
+            List<int> result = new List<int>();
+
+            AddToList(root, result);
+
+            return result;
+        }
+
+        private void AddToList(TreeNode root, List<int> values)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            values.Add(root.val);
+            if (root.left != null)
+            {
+                AddToList(root.left, values);
+            }
+            if (root.right != null)
+            {
+                AddToList(root.right, values);
+            }
         }
     }
 }
