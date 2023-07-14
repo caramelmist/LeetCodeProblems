557ad513e46169bacbd3dcd07b672c264d6593da
Sidesh Jungbahadur <sidesh.jungbahadur@derivco.co.za>
2023-07-14 14:01:30 +0200

    Completed problems 21 and 2352

---
+++ LeetCodeProblems/Solved/Problem21.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Solved
+{
+  public class Problem21 : Solution
+  {
+    public void Run()
+    {
+      ListNode actual = MergeTwoLists(new ListNode(1, new ListNode(3, new ListNode(4))), new ListNode(1, new ListNode(2, new ListNode(4))));
+      ListNode expected = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))));
+      Console.WriteLine("Outcome: {0}  {1}", expected, actual);
+
+      actual = MergeTwoLists(new ListNode(1, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))), new ListNode(1, new ListNode(2, new ListNode(4))));
+      expected = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5, new ListNode(6))))))));
+      Console.WriteLine("Outcome: {0}  {1}", expected, actual);
+
+      actual = MergeTwoLists(new ListNode(1, new ListNode(3, new ListNode(4))), new ListNode(1, new ListNode(2, new ListNode(4, new ListNode(5, new ListNode(6))))));
+      expected = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5, new ListNode(6))))))));
+      Console.WriteLine("Outcome: {0}  {1}", expected, actual);
+
+      actual = MergeTwoLists(new ListNode(), new ListNode());
+      expected = new ListNode();
+      Console.WriteLine("Outcome: {0}  {1}", expected, actual);
+
+      actual = MergeTwoLists(new ListNode(), new ListNode(0));
+      expected = new ListNode(0);
+      Console.WriteLine("Outcome: {0}  {1}", expected, actual);
+    }
+
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+      if(list1 == null && list2 == null)
+      {
+        return null;
+      }
+
+      ListNode result = new ListNode();
+      ListNode ret = new ListNode();
+      result = ret;
+
+      while (list1 != null || list2 != null)
+      {
+        if (list1 == null)
+        {
+          ret.val = list2.val;
+          list2 = list2.next;
+        }
+        else if (list2 == null)
+        {
+          ret.val = list1.val;
+          list1 = list1.next;
+        }
+        else
+        {
+          if(list1.val <= list2.val)
+          {
+            ret.val = list1.val;
+            list1 = list1.next;
+          }
+          else
+          {
+            ret.val = list2.val;
+            list2 = list2.next;
+          }
+        }
+        if(list1 != null || list2 != null)
+        {
+          ret.next = new ListNode();
+          ret = ret.next;
+        }
+      }
+
+      return result;
+    }
+
+  }
+}
---
+++ LeetCodeProblems/Solved/Problem2352.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Solved
+{
+    public class Problem2352 : Solution
+    {
+        public void Run()
+        {
+            int actual = EqualPairs(new int[][] { new int[] { 3, 2, 1 }, new int[] { 1, 7, 6 }, new int[] { 2, 7, 7 } });
+            int expected = 1;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+
+            actual = EqualPairs(new int[][] { new int[] { 3, 1, 2, 2 }, new int[] { 1, 4, 4, 5 }, new int[] { 2, 4, 2, 2 }, new int[] { 2, 4, 2, 2 } });
+            expected = 3;
+            Console.WriteLine("Outcome: {0} {1}", expected, actual);
+        }
+
+        public int EqualPairs(int[][] grid)
+        {
+            int total = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                total += EqualPairs(grid[i], grid);
+            }
+
+            return total;
+        }
+
+        private int EqualPairs(int[] match, int[][] grid)
+        {
+            int total = 0;
+            for (int x = 0; x < match.Length; x++)
+            {
+                bool isSame = true;
+                for (int y = 0; y < match.Length; y++)
+                {
+                    if (match[y] != grid[y][x])
+                    {
+                        isSame = false;
+                        break;
+                    }
+                }
+                if (isSame)
+                {
+                    total++;
+                }
+            }
+            return total;
+        }
+    }
+}
